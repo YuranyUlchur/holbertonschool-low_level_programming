89f1b3e25dce4d30258c944fa2f33751eab849de
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-09 11:47:01 -0500

    add an

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float flotante;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(character));
-	printf("Size of a int: %ld byte(s)\n", sizeof(integer));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integer));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longint));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longerinteger));
 	printf("Size of a float: %ld byte(s)\n", sizeof(flotante));
