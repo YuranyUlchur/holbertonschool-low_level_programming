9716551e6713688960055ad905aa77724b612103
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-09 11:41:07 -0500

    delete type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,13 +7,11 @@ int main(void)
 {
 	char character;
 	int integer;
-	long longer;
 	long int longint;
 	long long int longerinteger;
 	float flotante;
 	printf("Size of a char: %ld byte(s)\n", sizeof(character));
 	printf("Size of a int: %ld byte(s)\n", sizeof(integer));
-	printf("Size of a long: %ld byte(s)\n", sizeof(longer));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longint));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longerinteger));
 	printf("Size of a float: %ld byte(s)\n", sizeof(flotante));
