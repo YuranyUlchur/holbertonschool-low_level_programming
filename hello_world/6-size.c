c7b6670f1b53b79d88cf2ad97e5eed331fe9f7dd
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-09 11:45:18 -0500

    add space

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 	long int longint;
 	long long int longerinteger;
 	float flotante;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(character));
 	printf("Size of a int: %ld byte(s)\n", sizeof(integer));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longint));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(longerinteger));
 	printf("Size of a float: %ld byte(s)\n", sizeof(flotante));
-
 
 	return (0);
 }
