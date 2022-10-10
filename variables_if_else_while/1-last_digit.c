2ab32ec080af3935ed22039b058213f4c189c638
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-10 14:41:58 -0500

    less and last

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,15 +8,18 @@
 int main(void)
 {
 	int n;
+	int ultimodigito;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int ultimodigito = n %10;
-	if(ultimodigito > 5)
+	ultimodigito = n % 10;
+	if (ultimodigito > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ultimodigito);
-	else if(ultimodigito == 0)
+	else if (ultimodigito == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ultimodigito);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ultimodigito);
-	
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n,
+		ultimodigito);
+
 	return (0);
 }
