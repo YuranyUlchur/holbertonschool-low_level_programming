36b90635059a3bbda9d74dacba259e98e8234118
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-10 19:18:47 -0500

    the alphabet in lowercase, and then in uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		char small = tolower(x);
+
+		putchar(small);
+	}
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+	return (0);
+}
