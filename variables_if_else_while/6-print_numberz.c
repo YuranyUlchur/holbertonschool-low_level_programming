d6cb98a6b946a4f093a847c3867dd1f9a05dcf46
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-10 21:55:12 -0500

    use the variable putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+
+}
+putchar('\n');
+return (0);
+}
