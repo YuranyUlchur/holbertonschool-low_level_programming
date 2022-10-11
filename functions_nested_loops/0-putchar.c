7f29d7310a30581ee04fd18fda8930e2937b4b07
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-11 15:35:03 -0500

    printf _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *main - Entry point
  *Return: Always 0 (Success)
@@ -6,8 +6,14 @@
 int main(void)
 {
 
-printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-
-return (0);
+	return (0);
 }
