d644e91b8bf5ace744498eee2e58030d11a7236b
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-11 15:59:52 -0500

    the alphabet, in lowercase, followed by a new line

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+	void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
