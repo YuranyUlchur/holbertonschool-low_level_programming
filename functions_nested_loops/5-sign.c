83e553cea37bc295640ba0a8d382d69ec8a08d2c
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-12 00:03:36 -0500

    the sign of a number.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_islower- Entry point
+ * @n: is the character received
+ *
+ * Return: Returns 1 and prints + if n is greater than zero
+Returns 0 and prints 0 if n is zero
+Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
