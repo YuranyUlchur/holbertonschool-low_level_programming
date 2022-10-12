33c4b8eb50b3943dee31a40ee2a335e6c04e9b55
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-11 23:32:14 -0500

    function that checks lowercase or uppercase

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_islower- Entry point
+ * @c: is the character received
+ *
+ * Return: Returns 1 if c is lowercase Returns 0 otherwise
+ */
+	int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+	if(c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+	return (0);
+}
