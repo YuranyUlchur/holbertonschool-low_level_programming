0db1972c2bed360bb61d677e3297a458946ab4c6
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-12 15:23:13 -0500

    prints every minute starting at 00 and ending at 59

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *jack_bauer- void every minute of jack bauer
+ *@h: value the hours
+ *@m: value the minutes
+ *Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int h;
+int m;
+
+	for (h = 0; h <= 23; h++)
+	for (m = 0; m <= 23; m++)
+
+{
+	_putchar(h / 10 + '0');
+	_putchar(h % 10 + '0');
+	_putchar(':');
+}
+
+{
+	_putchar(m / 10 + '0');
+	_putchar(m % 10 + '0');
+	_putchar('\n');
+}
+
+}
