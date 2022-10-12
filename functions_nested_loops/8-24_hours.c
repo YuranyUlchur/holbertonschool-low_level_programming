61dad266784643b3ae998052c22ad03ae834b0d5
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-12 15:45:07 -0500

    every minute of Jack Bauer's day, starting at 00:00 until 23:59

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,18 +11,18 @@ int h;
 int m;
 
 	for (h = 0; h <= 23; h++)
+{
 	for (m = 0; m <= 23; m++)
 
 {
 	_putchar(h / 10 + '0');
 	_putchar(h % 10 + '0');
 	_putchar(':');
-}
-
-{
 	_putchar(m / 10 + '0');
 	_putchar(m % 10 + '0');
 	_putchar('\n');
+
+}
 }
 
 }
