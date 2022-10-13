6e3bac83326c5d35476ef533283d9b72ce360add
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-13 12:03:33 -0500

    that prints the 9 times table, starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,23 +1,47 @@
 #include "main.h"
 /**
- *times_table- void every minute of jack bauer
+ *times_table - prints the 0-9 times table
  */
+
 void times_table(void)
 {
-	int o;
-	int f;
-	int c;
+int filas;
+int colum;
+int c;
 
+c = 0;
 
-	for (f = 1; f < 9; f++){
-	for (c = 0; c < 9; c++)
-	{
-		o = f * c;
-		_putchar(o + )
+{
+for (filas = 0; filas <= 9; filas++)
+{
+for (colum = 0; colum <= 9; colum++)
+{
 
-	}
-	
-	}
+int operation;
 
-	
- }
+operation = c *colum;
+
+if (operation > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((operation / 10) + '0');
+_putchar((operation % 10) + '0');
+}
+else if (colum == 0)
+{
+_putchar(operation + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(operation + '0');
+}
+}
+c++;
+_putchar('\n');
+}
+}
+}
