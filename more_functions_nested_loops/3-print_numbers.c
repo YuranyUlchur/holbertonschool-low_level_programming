56a44c17574d806f4575a6ed03eb92aaf6cd7671
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 15:12:33 -0500

    delete line

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,15 @@
 #include "main.h"
 /**
  *print_numbers- printf the numbers
- *@x: value
  *Return: Return the numbers, from 0 to 9
  */
 void print_numbers(void)
-
+int x;
 {
-	int x;
+	
 	for (x = 0; x <= 9; x++)
-	{
-		_putchar('\n');
-		_putchar(x);
-
-	}
-
-
+{
+	_putchar('\n');
+	_putchar(x);
+}
 }
