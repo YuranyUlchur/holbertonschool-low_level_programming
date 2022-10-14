5630f4ed0cbea943f95c39e5c449d75be2d540c2
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 15:09:04 -0500

    function prints the numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers- printf the numbers
+ *Return: Return the numbers, from 0 to 9
+ */
+void print_numbers(void)
+
+{
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('\n');
+		_putchar(x);
+
+	}
+
+
+}
