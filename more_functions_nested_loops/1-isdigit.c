9f155ee81deafc9189867bce373602fcd756dfff
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 13:33:28 -0500

    function that checks a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_isupper- Returns a copy of this string converted to uppercase
+ *@c: Returns 1 if c is uppercase
+ *Return: Returns 1 if c is lowercase Returns 0 otherwise
+ */
+int _isdigit(int c)
+
+{
+{
+	if (c >= 'a' && c <= 'z')
+{
+	return (0);
+}
+	if (c >= 0 && c <= 9)
+{
+	return (1);
+}
+	return (1);
+}
+}
