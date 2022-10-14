3008870930db307e7820e2fc2b02b943a3bafb06
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 13:44:10 -0500

    change the order of the if

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_isdigit- returns a nonzero value if c is a decimal digit 
+ *_isdigit- returns a nonzero value if c is a decimal digit
  *@c: Returns 1 if c is uppercase
  *Return: Returns 1 if c is a digit Returns 0 otherwise
  */
@@ -8,13 +8,13 @@ int _isdigit(int c)
 
 {
 {
-	if (c >= 'a' && c <= 'z')
-{
-	return (0);
-}
 	if (c >= 0 && c <= 9)
 {
 	return (1);
+}
+	if (c >= 'a' && c <= 'z')
+{
+	return (0);
 }
 	return (1);
 }
