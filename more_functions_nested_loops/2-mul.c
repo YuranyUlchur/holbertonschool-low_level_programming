2075319fcb9848ec53c4dfa088ee8a0cd749b1f8
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 14:46:41 -0500

    function multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *mul- returns a nonzero value if c is a decimal digit
+ *@a: value
+ *@b: value
+ *Return: multiplies
+ */
+int mul(int a, int b)
+
+{
+	int operacion;
+
+	operacion = a * b;
+	return (operacion);
+
+}
