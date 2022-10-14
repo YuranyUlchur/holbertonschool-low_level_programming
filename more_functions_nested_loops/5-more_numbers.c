00767d86806ab1ffa5b2de1ee7aff093b4096032
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 16:40:31 -0500

    function  prints times the numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -4,19 +4,23 @@
  */
 void more_numbers(void)
 {
-{
+
 	int x;
-	int j;
+	int n;
 
-	j = *10;
-	{
+	for (n = 0 ; n <= 9; n++)
+{
 	for (x = 0 ; x <= 14; x++)
-	{
-	_putchar(x);
-	_putchar(j);
 
-	}
+{
+
+	if (x >= 10)
+
+{
+	_putchar((x / 10) + '0');
+}
+	_putchar((x % 10) + '0');
+}
 	_putchar('\n');
-	}
-
+}
 }
