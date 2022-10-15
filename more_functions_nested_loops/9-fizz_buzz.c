20af306cb2d752dfd892b22b27679c92848ac44f
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-14 22:43:10 -0500

    fix spaces and new line

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,18 +10,22 @@ for (i = 1; i < 101 ; i++)
 {
 	if (i % 3 == 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	if (i % 5 == 0)
 	{
-		printf("Buzz ");
+		printf("Buzz");
 	}
 	if (i % 3 != 0 && i % 5 != 0)
 	{
-		printf("%d ", i);
+		printf("%d", i);
+	}
+	if (i < 100)
+	{
+		printf(" ");
 	}
 	
 }
-
+	printf("\n");
 	return (0);
 }
