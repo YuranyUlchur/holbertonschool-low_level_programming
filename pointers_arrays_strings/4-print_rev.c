8c00679146913b33535a2788e5fb43d65fdef2b3
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-17 22:13:43 -0500

    function prints a string, in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,21 @@
  *@s: value pointers
 
  */
-void print_rev(char *s);
-
-
+void print_rev(char *s)
 
 {
+	int i;
+	int x = 0;
 
-	
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	for (i = x - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
 }
