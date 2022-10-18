17ad05e77f858bf7d3fffc43e27b8d4b13604aea
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-17 20:48:50 -0500

    function takes a string as an argument and returns its length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - takes a string as an argument and returns its length
+ *@s: value pointers
+ *Return: Return value i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
