b8599cd608145744553e02594ebea99fec446f03
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-19 13:57:04 -0500

    chain concatenates

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *_strncat -  chain concatenates
+ *@dest: value pointers
+ *@src: value pointers
+ *@n: value
+ *Return: Return value poiners dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i;
+	int o = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+
+	{
+	}
+	for (o = 0; src[o] != '\n'; o++)
+	{
+		if (o < n)
+		{
+
+			dest[i + o] = src[o];
+
+			o++;
+		}
+
+		dest[i + o] = '\0';
+
+	}
+	_putchar('\n');
+
+	return (dest);
+
+}
