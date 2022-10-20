bd1f9aedd144dde7fd1593fab09eef669562e334
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-20 13:13:25 -0500

    capitalizes all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *cap_string- function is used capitalizes all words of a string
+ *@pun: value pointers
+ *Return: Return value pointers
+ */
+char *cap_string(char *pun)
+
+{
+	int i;
+	int num = 32;
+
+	for (i = 0; pun[i] != '\0'; i++)
+	{
+		if (pun[i] >= 'a')
+			pun[i] = pun[i] - num;
+	}
+	return (pun);
+}
