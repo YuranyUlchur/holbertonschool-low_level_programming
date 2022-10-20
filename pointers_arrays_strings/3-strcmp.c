724639bfae76bb84481b8187b405b09d6f10e34e
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-19 22:01:53 -0500

    compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,8 @@
 
 /**
  *_strcmp -   compares two strings
- *@dest: value pointers
- *@src: value pointers
- *@n: value
+ *@s1: value pointers
+ *@s2: value pointers
  *Return: Return value poiners dest
  */
 int _strcmp(char *s1, char *s2)
@@ -14,13 +13,13 @@ while (*s1 != '\0' || *s2 != '\0')
 {
 	if (*s1 != *s2)
 {
-	return(*s1 - *s2);
+	return (*s1 - *s2);
 }
 	s1++;
 	s2++;
 
 }
 
-return(0);
+return (0);
 
 }
