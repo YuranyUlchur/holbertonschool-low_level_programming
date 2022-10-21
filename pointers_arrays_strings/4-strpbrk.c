972c417e3e840261b50f8a42bd6091c3a6e03a31
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-21 11:51:17 -0500

    string for any of a set of bytes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strpbrk -   string for any of a set of bytes
+ *@s: value pointers
+ *@accept: value pointers
+ *Return: Return value 
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int x;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; accept[x]; x++)
+		if (s[i] == accept[x])
+		{
+
+			return (s + i);
+		}
+	}
+	if (s[i] == accept[x])
+	{
+
+		return (s + i);
+	}
+	return (0);
+}
