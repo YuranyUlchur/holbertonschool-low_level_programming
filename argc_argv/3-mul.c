bfb60582d51689e716f969a65d443ea25a24e056
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-26 15:34:15 -0500

    multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,5 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int oneinteger;
+	int twointeger;
+
+	if (argc == 3)
+	{
+		oneinteger = atoi(argv[1]);
+		twointeger = atoi(argv[2]);
+
+		printf("%d\n", oneinteger * twointeger);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char c);
 int main(int argc, char *argv[]);
-
+int atoi(const char *nptr);
 
 #endif
