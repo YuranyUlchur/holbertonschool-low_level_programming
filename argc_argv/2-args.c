5605454399d701fe93b7c18c7cb8c2df4b142c96
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-26 15:00:43 -0500

    prints all arguments it receives

---
+++ argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main- Entry point
+ *@argc: value integer
+ *@argv: pointers char
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
