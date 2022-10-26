eb80c13285664823af89cd8e4b2c3ecb00ce298e
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-25 21:17:38 -0500

    add changes

--- static_libraries/_atoi.c
+++ static_libraries/_atoi.c
@@ -5,5 +5,5 @@
  */
 int _atoi(char *s)
 {
-
+	return (0);
 }
--- static_libraries/main.c
+++ static_libraries/main.c
@@ -5,7 +5,3 @@ int main(void)
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
 }
-julien@ubuntu:~/Static Librairies$ gcc -std=gnu89 main.c -L. -lmy -o quote
-julien@ubuntu:~/Static Librairies$ ./quote 
-"At the end of the day, my goal was to be the best hacker"
-    - Kevin Mitnick
