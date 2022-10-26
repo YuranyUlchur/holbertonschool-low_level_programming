48287bd13b7fba2c5ee6342130498d1d58e0fc9c
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-26 11:44:43 -0500

    program prints its name, the argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main- Entry point
+ *@argc: value integer
+ *@argv: pointers char
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
