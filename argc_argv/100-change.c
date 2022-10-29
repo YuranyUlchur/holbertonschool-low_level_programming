7d4a7e63d26f91176a7aebe309a5f33fa22baccc
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-28 21:15:46 -0500

    add advanced

---
+++ argc_argv/100-change.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main- Entry point
+ *@argc: value integer
+ *@argv: pointers char
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int monedas = [25, 10, 5, 2, 1]
+	int i;
+	int cent
+
+
+	for (i = 1; i < count; i++)
+	{
+		/* code */
+	}
+	
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+
+}
