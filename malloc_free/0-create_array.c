072a49dd6b59728331bde52b83266822d893f5f2
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-31 10:54:43 -0500

    Dynamic memory allocation

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *create_array - Entry point
+ *@size: value type unsigned integer
+ *@c: value tipe char
+ *Return: type char
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *puntero;
+
+	puntero = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		puntero[i] = c;
+	}
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+
+	return (puntero);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+Repository C - malloc, free
