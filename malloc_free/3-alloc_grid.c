8853dc3d0f9d585aa12acf08a116fcf3dcb1e1a2
YuranyUlchur <ulchuryurani80@gmail.com>
2022-10-31 17:29:08 -0500

    frees a 2 dimensional grid previously

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strdup - Entry point
+ *@str: value type unsigned integer
+ *Return: type char
+ */
+int **alloc_grid(int width, int height)
+{
+	
+}
---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_grid -  frees a 2 dimensional grid previously
+ *@grid: value pointers
+ *@height: value integer
+ *Return: type char
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
