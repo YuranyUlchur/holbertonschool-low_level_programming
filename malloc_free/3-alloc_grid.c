c697581e5631e100f03a3109ae09f06a7198e5c7
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-01 09:52:26 -0500

    pointer to a 2 dimensional array of integers

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *_strdup - Entry point
- *@str: value type unsigned integer
+ *alloc_grid- Entry point
+ *@width: value type integer
+ *@height: value type integer
  *Return: type char
  */
 int **alloc_grid(int width, int height)
 {
-	
+	int **puntero;
+	int i;
+	int traverse;
+	int traverse2;
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+	puntero = malloc(height * sizeof(int *));
+	if (**puntero != '\0')
+	{
+		for (i = 0; i < height; i++)
+		{
+			puntero[i] = malloc(width * sizeof(int *));
+			if (*puntero[i] == '\0')
+			{
+				for (; i >= 0; i--)
+				{
+					free(puntero[i]);
+				}
+				free(puntero);
+				return ('\0');
+			}
+		}
+		for (traverse = 0; traverse < height; traverse++)
+		{
+			for (traverse2 = 0; traverse2 < width; traverse2++)
+			{
+				puntero[traverse][traverse2] = 0;
+			}
+		}
+		return (puntero);
+	}
+	return ('\0');
 }
