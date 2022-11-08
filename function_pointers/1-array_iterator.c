ea962e3499c7bba4bf3fb92f76c6fd93eb23b163
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-08 17:51:34 -0500

    funcion array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,16 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL)
+	if (array != NULL || action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
-
 			action(array[i]);
-		}
-		if (array[i] != '\0')
-		{
-
 		}
 	}
 }
