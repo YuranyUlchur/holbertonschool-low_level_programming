904a697ac334eb6b9a62983f0b26375b33c5d278
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-08 12:20:07 -0500

    delete space

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,27 +15,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int lenowner, lenname, i, o;
 	dog_t *newdog;
 
-	
-	
 		newdog = malloc(sizeof(dog_t));
 		lenname = strlen(name);
 		lenowner = strlen(owner);
 
-		if (newdog == NULL)
-		{
-			return (NULL);
-		}
 		newdog->name = malloc(lenname + 1);
 		newdog->owner = malloc(lenowner + 1);
 		if (newdog->name == NULL || newdog->owner == NULL)
 		{
 			free(newdog);
-			free(name);
-			free(owner);
+			free(newdog->name);
+			free(newdog->owner);
 			return (NULL);
 		}
-		
-		
 		for (i = 0; i < lenname + 1; i++)
 		{
 			newdog->name[i] = name[i];
