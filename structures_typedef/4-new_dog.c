a739ab84133fab5c394be21e19097c7d5df3effd
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-08 12:24:41 -0500

    add null a newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,6 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newdog;
 
 		newdog = malloc(sizeof(dog_t));
+		if (newdog == NULL)
+		{
+			return (NULL);
+		}
 		lenname = strlen(name);
 		lenowner = strlen(owner);
 
