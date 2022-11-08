f64699af2b38bbed69e36f30d00956c6ce188ffd
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-08 12:32:38 -0500

    free dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * free_dog - function frees dogs
@@ -8,9 +9,9 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 	{
-		free(d)
-		free(d->name)
-		free(d->owner)
+		free(d);
+		free(d->name);
+		free(d->owner);
 	}
 	
 	
