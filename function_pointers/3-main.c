10a5752ab342318436f567e69aa25cba6b4c3577
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-09 22:22:20 -0500

    create the calcultor

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num1 == 0) || (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
