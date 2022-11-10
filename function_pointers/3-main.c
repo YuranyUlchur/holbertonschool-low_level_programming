ea985a7b1465086683994c59b51b68c3f5405de3
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-09 22:25:14 -0500

    fix conditional

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,16 +12,16 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	op = argv[2];
-
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
