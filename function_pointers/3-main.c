6ae13020c89b816b56f469e0832da97cd0ef3b41
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-09 21:53:34 -0500

    create a calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function main
+ * @argc: value integer
+ * @argv: value pointers
+ * Return: Return integer
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit("99");
+	}
+	if ((*op '/' && num1 == 0) || (*op '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit("100");
+	}
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "3-calc.h"
+/**
+ * op_add - function suma
+ * @a: value integer
+ * @b: value integer
+ * Return: Return the suma
+ */
+
+int op_add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+#include "3-calc.h"
+
+/**
+ * op_sub - function  the difference
+ * @a: value integer
+ * @b: value integer
+ * Return: Return the resta
+ */
+
+int op_sub(int a, int b)
+{
+	int resta;
+
+	resta = a - b;
+	return (resta);
+}
+#include "3-calc.h"
+
+/**
+ * op_mul - function the multiplicacion
+ * @a: value integer
+ * @b: value integer
+ * Return: Return the multiplicacion
+ */
+
+int op_mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+#include "3-calc.h"
+/**
+ * op_div - function the division
+ * @a: value integer
+ * @b: value integer
+ * Return: Return the division
+ */
+int op_div(int a, int b)
+{	int div;
+
+	div = a / b;
+	return (div);
+}
+#include "3-calc.h"
+/**
+ * op_mod - function the remainder of the division
+ * @a: value integer
+ * @b: value integer
+ * Return: Return the remainder of the division
+ */
+
+int op_mod(int a, int b)
+{	int remainder;
+
+	remainder = a % d;
+	return (remainder);
+}
