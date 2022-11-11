a11ce549cdf54c51b9c751c4ecd03f743124eae2
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-10 22:57:44 -0500

    delete space

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,8 +13,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *pointer;
 
-	va_list string;
 	va_start(string, n);
+	va_list string;
+
 
 	while (i < n)
 	{
