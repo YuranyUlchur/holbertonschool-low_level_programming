ecd6bf74f4024ced3ce8e8937443e96d090d2583
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-29 18:35:07 -0500

    function new node at the beginning of a list.

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * *add_node - function new node at the beginning of a list.
+ * @head: pointer const lis_t
+ * @str: const char pointer
+ * Return: prints all the elements of a list_t list.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node, *last = (*head);
+
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+	}
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	node->str = strdup(str);
+	node->len = i;
+	node->next = NULL;
+
+	if ((*head) == NULL)
+	{
+		(*head) = node;
+		return (node);
+	}
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = node;
+	return (node);
+}
