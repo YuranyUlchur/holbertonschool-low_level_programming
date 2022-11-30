21dd08e9b2ee5d81167f9013579138f543ebc85f
YuranyUlchur <ulchuryurani80@gmail.com>
2022-11-29 19:29:11 -0500

    free head

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
 /**
- * free_list - function  that frees a list_t
+ * free_list - function that frees a list_t
  * @head: pointer const lis_t
  */
 void free_list(list_t *head)
 {
-	if (head->str == NULL)
+	while (head == NULL)
+	free(head->next);
+	free(head->str);
 	free(head);
 }
