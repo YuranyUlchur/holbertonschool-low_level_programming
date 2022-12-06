847082e57c05b25ee012156f80a5875c81105345
YuranyUlchur <ulchuryurani80@gmail.com>
2022-12-06 15:08:20 -0500

    add return the condition if

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,6 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node->n = n;
 	if (idx == 0)
 	{
+		return (add_dnodeint(h, n));
 		node->next = aux;
 		*h = node;
 		return (node);
