7323f2c46d0772d154c6362ac12d81aa06674551
YuranyUlchur <ulchuryurani80@gmail.com>
2022-12-06 15:04:35 -0500

    changes the function that inserts a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,8 +18,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	node->n = n;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-	aux = *h;
+	{
+		node->next = aux;
+		*h = node;
+		return (node);
+	}
 	while (i < idx - 1)
 	{
 		if (!aux)
