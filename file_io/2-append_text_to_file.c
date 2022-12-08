6e43e8813a0651205976e2d80381d2c98170d9c4
YuranyUlchur <ulchuryurani80@gmail.com>
2022-12-08 17:53:31 -0500

    add O_APPEND

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file -  function that creates a file.
+ *@text_content: variable char
+ *@filename: pointer const char
+ *Return: ssize_t
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = 0;
+
+	if (text_content)
+		len = strlen(text_content);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
---
+++ file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * create_file -  function that creates a file.
+ *@text_content: variable char
+ *@filename: pointer const char
+ *Return: ssize_t
+ */
+int main(int argc, char **argv)
+{
+	int fd = 0, fd_to = 0, close_status = 0, i;
+	size_t bytes = 1;
+	char *file_from = NULL, file_to = NULL;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file file_to\n");
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd = open(file_from, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd_to = open(file_to, 0_CREAT | 0_RWR | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd);
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", file_to);
+		exit(99);
+	}
+	for (i = 0; i < 1024; i++)
+	{
+		buff[i] = '\0';
+	}
+
+	while (bytes)
+	{
+		bytes = read(fd, buff, 1024);
+		write(fd_to, buff, bytes);
+	}
+	close_status = close(fd);
+	if (close_status == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
