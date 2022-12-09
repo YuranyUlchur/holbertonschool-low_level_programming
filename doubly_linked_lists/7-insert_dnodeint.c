2e6ba87ba7c42fdb74f12cb1c02b0f97fe2b6f25
YuranyUlchur <ulchuryurani80@gmail.com>
2022-12-09 15:57:33 -0500

    function that inserts a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,44 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - function that inserts a new node
- * @h: pointer dlistint_t
- * @idx:  is the index of the list where the new node
- * @n: integer
- * Return: return dlistint_t
+ **insert_dnodeint_at_index -  function that inserts a new node
+ *@h: variable dlistint_t
+ *@idx: variable unsigned integer
+ *@n: variable integer
+ *Return: dlistint_t
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *node, *aux = *h;
-	unsigned int i = 0, j = 0;
+	dlistint_t *new_node, *aux = *h;
+	unsigned int counter = 0;
 
-	while (aux)
-	{
-		j++;
-		aux = aux->next;
-	}
-	if (!*h && idx != 0)
+	if (!h || (!*h && idx != 0))
 		return (NULL);
-	if (j < idx)
-		return (NULL);
-	node = malloc(sizeof(dlistint_t));
 
-	if (!node)
-		return (NULL);
-	node->n = n;
-	node->next = NULL;
-	aux = *h;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (i < idx - 1)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	while (aux->next || counter == idx)
 	{
+		if (counter == idx)
+		{
+			aux->prev->next = new_node;
+			new_node->prev = aux->prev;
+
+			new_node->next = aux;
+			aux->prev = new_node;
+
+			return (new_node);
+		}
+		counter++;
 		aux = aux->next;
-		i++;
 	}
+	if (counter + 1 == idx)
+	{
+		new_node->next = NULL;
 
-	node->next = aux->next;
-	aux->next = node;
-
-	return (node);
+		aux->next = new_node;
+		new_node->prev = aux;
+		return (new_node);
+	}
+	free(new_node);
+	return (NULL);
 }
