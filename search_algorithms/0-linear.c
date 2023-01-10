6d29ef1c0e332f358a3a78b384f5fe112e6cc820
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-10 16:00:13 -0500

    create  function Linear search algorithm

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+#include "search.h"
+#include <stdio.h>
+/**
+ * linear_search-  function Linear search algorithm
+ *@array: value pointers
+ *@size: value size
+ *@value: value integer
+ *Return: return interger
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	for (i = 0; i < size; i++)
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+	if (array[i] == value)
+		return i;
+	return (-1);
+}
